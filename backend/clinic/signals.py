f81144df0fd26db0992bae6f4b73bb47c603f3e7
RiadBensemmane <riad200339@gmail.com>
2024-07-24 13:15:37 +0100

    added a signal to automatically populate the Tooth table post migrations

--- backend/clinic/signals.py
+++ backend/clinic/signals.py
@@ -9,12 +9,13 @@
 #         raise ValidationError('The treatment does not belong to the selected patient.')
 
 
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_migrate
 from django.dispatch import receiver
 from .models import Payment, Appointment
 from django.utils import timezone
 from notification.models import PaymentNotification, ContentType
 from django.contrib.auth import get_user_model
+from clinic.models import Tooth
 
 User = get_user_model()
 
@@ -53,3 +54,11 @@ User = get_user_model()
 #         for receptionist in User.objects.filter(role='receptionist'):
 #             PaymentNotification.objects.create(user=receptionist, model_id=ContentType.objects.get_for_model(Payment), row_id=instance.id)
 #     PaymentNotification.objects.create(user=instance.dentist,model_id=ContentType.objects.get_for_model(Payment), row_id=instance.id)
+
+@receiver(post_migrate)
+def create_teeth(sender, **kwargs):
+    if sender.name == 'clinic':
+        tooth_codes = [f"{x}{y}" for x in range(1, 5) for y in range(1, 9) if not (x == 2 and y > 8) and not (x == 4 and y > 8)]
+        teeth_to_create = [Tooth(code=code) for code in tooth_codes]
+        Tooth.objects.bulk_create(teeth_to_create, ignore_conflicts=True)
+        print('Snan t3amerou b1')
